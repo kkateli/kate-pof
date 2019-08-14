df025508b3fceb4ed4d7e00520d1de6117e72385
kkateli <kateli24@outlook.com>
2019-08-15 11:56:03 +1200

    fixed scroll position

--- portfolio/src/components/scrollLabel/ScrollLabel.js
+++ portfolio/src/components/scrollLabel/ScrollLabel.js
@@ -1,29 +1,33 @@
-import React,{Component} from "react";
+import React, { Component } from "react";
 import arrow from "../../assets/images/arrow.png";
-import scrollWord from "../../assets/images/scrollWord.png"; 
+import scrollWord from "../../assets/images/scrollWord.png";
 import "./ScrollLabel.css";
-import {flash} from "react-animations";
-import Radium, {StyleRoot} from 'radium';
-const styles = {
-    flash: {
-      animation: '2s',
-      animationName: Radium.keyframes(flash, 'flash')
-    }
-  }
+import { flash } from "react-animations";
+import Radium, { StyleRoot } from "radium";
 
-class ScrollLabel extends Component{
-    render(){
-        return(
-            <div className="scrollWraper"> 
+const styles = {
+  flash: {
+    animation: "2s",
+    animationName: Radium.keyframes(flash, "flash"),
     
-            <StyleRoot>
-            <img className="scrollArrow" src={arrow} alt="arrow" />
-            <img className="scrollWord" src={scrollWord} alt="word" style={styles.flash}/>  
-            </StyleRoot> 
-            
-            </div>
-                )
-    }
-    
+  }
+};
+
+class ScrollLabel extends Component {
+  render() {
+    return (
+      <div className="scrollWraper">
+        <StyleRoot>
+          <img className="scrollArrow" src={arrow} alt="arrow" />
+          <img
+            className="scrollWord"
+            src={scrollWord}
+            alt="word"
+            style={styles.flash}
+          />
+        </StyleRoot>
+      </div>
+    );
+  }
 }
 export default ScrollLabel;
