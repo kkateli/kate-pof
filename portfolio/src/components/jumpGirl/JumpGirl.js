a270dc341498e4bf2e741e917b72ff752a5cdffa
kkateli <kateli24@outlook.com>
2019-08-15 15:20:23 +1200

    Added cloud

--- portfolio/src/components/jumpGirl/JumpGirl.js
+++ portfolio/src/components/jumpGirl/JumpGirl.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import jump from "../../assets/images/fall.png";
+import cloudBig1 from "../../assets/images/cloudBig.png";
+import cloudBig2 from "../../assets/images/cloudBig.png";
 import { TweenMax, TimelineMax } from "gsap/TweenMax";
 // import TimelineMax from "gsap";
 import ScrollMagic from "ScrollMagic";
@@ -8,6 +10,7 @@ import "scrollmagic/scrollmagic/uncompressed/plugins/animation.velocity";
 import "scrollmagic/scrollmagic/uncompressed/plugins/debug.addIndicators";
 
 import "./JumpGirl.css";
+
 class JumpGirl extends Component {
   state = {
     controller: new ScrollMagic.Controller()
@@ -19,20 +22,43 @@ class JumpGirl extends Component {
       duration: "500" // hide 10% before exiting view (80% + 10% from bottom)
     })
       .setClassToggle("#reveal1", "visible") // add class to reveal
+      // .setClassToggle("#reveal2", "visible2") // add class to reveal
       .setPin("#reveal1")
-      .addIndicators() // add indicators (requires plugin)
+      .addIndicators({name:"jumpGirl"}) // add indicators (requires plugin)
       .addTo(this.state.controller);
+
+      new ScrollMagic.Scene({
+        triggerElement: "#trigger2",
+        triggerHook: 0.5, // show, when scrolled 10% into view
+        duration: "500",
+        offset:-880 // hide 10% before exiting view (80% + 10% from bottom)
+      })
+        
+        .setClassToggle("#reveal2", "visible2") // add class to reveal
+        .addIndicators({name:"cloud"}) // add indicators (requires plugin)
+        .addTo(this.state.controller);
+
+    
   }
   render() {
     return (
       <div>
-        <div style={{ height: "500px", background: "orange" }}>
-          <div class="spacer s2" />
+        <div style={{ height: "500px", background: "#7ec0ee" }}>
+        <div class="spacer s2" />
           <div id="trigger1" class="spacer s0" />
           <div id="reveal1" class="box2 blue">
             <img src={jump} alt="jump girl" />
           </div>
+
           <div class="spacer s2" />
+          <div id="trigger2" class="spacer s0" />
+          <div id="reveal2" class="spacer s0">
+            <img className="cloudBig1" src={cloudBig1} alt="Cloud Big1" />
+            <img className="cloudBig2" src={cloudBig2} alt="Cloud Big2" />
+          </div>
+
+         
+          
         </div>
       </div>
     );
