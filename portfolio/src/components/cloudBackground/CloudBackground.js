efa0aca62bf77332a34cbfecb910b164ab069a3f
kkateli <kateli24@outlook.com>
2019-08-26 14:23:14 +1200

    Added horizontal scroll effect

--- portfolio/src/components/cloudBackground/CloudBackground.js
+++ portfolio/src/components/cloudBackground/CloudBackground.js
@@ -1,29 +1,16 @@
 import React, { Component } from "react";
 import cloudBig from "../../assets/images/cloudBig.png";
-import { TweenMax, TimelineMax } from "gsap/TweenMax";
-// import TimelineMax from "gsap";
-import ScrollMagic from "ScrollMagic";
-import "scrollmagic/scrollmagic/uncompressed/plugins/animation.gsap";
-import "scrollmagic/scrollmagic/uncompressed/plugins/debug.addIndicators";
 import "./CloudBackground.css";
 class CloudBackground extends Component {
-  state = {
-    controller: new ScrollMagic.Controller()
-  };
-  componentDidMount() {
-    new ScrollMagic.Scene({ triggerElement: "#pin2" , triggerHook:0.65})
-      .setPin("#pin2")
-    //   .addIndicators({ name: "2 (duration: 0)" }) // add indicators (requires plugin)
-      .addTo(this.state.controller);
-  }
+  
   render() {
     return (
       <div>
         <div class="spacer s2" />
         <div id="trigger2" class="spacer s0" />
         <div id="pin2">
-          <img className="cloudBackground1" src={cloudBig} alt="cloud big" />
-          <img className="cloudBackground2" src={cloudBig} alt="cloud big" />
+          <img className="cloudBackground" src={cloudBig} alt="cloud big" />
+          <img className="cloudBackground" src={cloudBig} alt="cloud big" />
         </div>
         <div class="spacer s2" />
       </div>
--- portfolio/src/components/structure/Structure.js
+++ portfolio/src/components/structure/Structure.js
@@ -1,63 +1,39 @@
 import React, { Component } from "react";
 import "./Structure.css";
 import $ from "jquery";
-import { TweenMax, TimelineMax } from "gsap/TweenMax";
-// import TimelineMax from "gsap";
-import ScrollMagic from "ScrollMagic";
-import "scrollmagic/scrollmagic/uncompressed/plugins/animation.gsap";
-import "scrollmagic/scrollmagic/uncompressed/plugins/debug.addIndicators";
 
 class Structure extends Component {
-  state = {
-    controller: new ScrollMagic.Controller()
-  };
-
+  
   componentDidMount() {
-    let $target = $(".slider").first(),
-      currentPosition = parseInt($target.css("transform").split(",")[4]),
-      moveBy = function(scrolledBy) {
-        currentPosition += scrolledBy;
-        $target.css("transform", "translateX(" + currentPosition + "px)");
-      },
-      lastScrollTop = 0;
-
-    currentPosition = isNaN(currentPosition) ? 0 : currentPosition;
-    $(window).bind("scroll", function(e) {
-      var scrolledBy = $(window).scrollTop() - lastScrollTop;
-      moveBy(-scrolledBy);
-      lastScrollTop = $(window).scrollTop();
+   
+    var divh = $("#dummy").position().top;
+    // calculating positions
+    $(window).scroll(function(e) {
+      var top = $(window).scrollTop();
+      if (top > divh) {
+        $("#dummy > .in-scroll").css(
+          "transform",
+          "translateX(-" + (top - divh) + "px)"
+        );
+        $("#dummy").css({
+          position: "fixed",
+          top: "0px"
+        });
+      } else {
+        $("#dummy").css({
+          top: "2000px",
+          position: "absolute"
+        });
+      }
     });
-
-    new ScrollMagic.Scene({
-      triggerElement: "#triggerWalker",
-      triggerHook: 0.5,
-      offset: -1000
-    })
-      .setPin("#walker")
-      .addIndicators({name:"walker"}) // add indicators (requires plugin)
-      .addTo(this.state.controller);
   }
   render() {
     return (
       <div>
-        <div class="spacer s2" />
-        <div id="triggerWalker" class="spacer s0" />
-
-        <div id="walker">
-          <div class="slider">
-            <div>
-              {" "}
-              <div class="slide" />
+        <div class="hori-scroll" id="dummy">
+          <div class="in-scroll" >
+           <p>Start</p>
             </div>
-            <div>
-              {" "}
-              <div class="slide" />
-            </div>
-            <div>
-              {" "}
-              <div class="slide" />
-            </div>
-          </div>
         </div>
       </div>
     );
