5ac6359c8f8c4873a99438cdeaea24dabe435378
kkateli <kateli24@outlook.com>
2019-08-29 21:38:53 +1200

    Changed button

--- portfolio/src/components/contact/Contact.js
+++ portfolio/src/components/contact/Contact.js
@@ -2,6 +2,7 @@ import React, {Component} from "react";
 import {Form, Button} from "react-bootstrap";
 import "./Contact.css";
 import thankyou from "../../assets/images/thankyou.png";
+import send from '../../assets/images/send.png';
 class Contact extends Component{
     render(){
         return(
@@ -25,9 +26,8 @@ class Contact extends Component{
     <Form.Label>Comments</Form.Label>
     <Form.Control as="textarea" rows="4" />
   </Form.Group>
-  <Button type="submit">
-    Submit
-  </Button>
+  
+  <img src={send} alt="send" className="send"/>
 </Form>
 <img src={thankyou} alt="thankyou" className="thankyou"/>
 </div>
