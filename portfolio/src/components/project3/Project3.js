119ebffcb1f30b7568934163fbb24c14b4ecd027
kkateli <kateli24@outlook.com>
2019-09-01 11:51:49 +1200

    Changed the third project

--- portfolio/src/components/project3/Project3.js
+++ portfolio/src/components/project3/Project3.js
@@ -1,25 +1,16 @@
 import React from "react";
-import p3p1 from "../../assets/images/p3/1.png";
-import p3p2 from "../../assets/images/p3/2.png";
-import p3p3 from "../../assets/images/p3/3.png";
+import house4 from "../../assets/images/house4.png";
+import house5 from "../../assets/images/house5.png";
 import "./Project3.css";
-import { Col, Row } from "react-bootstrap";
 import bee from "../../assets/images/bee.png";
 const project3 = () => {
   return (
     <div className="project3">
+      <img src={house4} alt="house4" id="house4" />
+      <img src={house5} alt="house5" id="house5" />
        <img src={bee} alt="bee" className="bee3" />
-      <img className="p3p1" src={p3p1} alt="p1" />
-      
-      <Row>
-        <Col>
-          <img src={p3p2} alt="p3p2" className="p3Table" />
-        </Col>
-        <Col>
-          <img src={p3p3} alt="p3p3" className="p3Table" />
-        </Col>
-      </Row>
      
+     <div id="base4" />
     </div>
   );
 };
