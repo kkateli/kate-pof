c4f2cf1663f081cbeeebf9205b11d516b574d547
kkateli <kateli24@outlook.com>
2019-09-01 11:06:12 +1200

    Changed the second project

--- portfolio/src/components/project2/Project2.js
+++ portfolio/src/components/project2/Project2.js
@@ -1,13 +1,14 @@
 import React from "react";
-import p2 from "../../assets/images/p2/1.png";
-import detail2 from "../../assets/images/p2/detail.png";
+import house2 from "../../assets/images/house2.png";
+import house3 from "../../assets/images/house3.png";
 import bee from "../../assets/images/bee.png";
 import "./Project2.css";
 const project2 =()=>{
     return(
         <div className="project2">
-            <img className="p2p1" src={p2} alt="p2" />
-            <img src={detail2} alt="detail" id="detail2"/>
+            <img src={house2} alt="house2" className="house2" />
+            <img src={house3} alt="house3" className="house3" />
+            <div id="base3" />
             <img src={bee} alt="bee" id="bee2"/>
 
         </div>
